2a1a533e7aa6776c0c4ead52c4afe4093e633191
HishamSaeed <hisham.saeed.92@gmail.com>
2026-08-28 16:30:42 +0200

    add qss and token file

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,28 @@
 #include <QApplication>
+#include <QFile>
+#include <QIODevice>
+#include <QTextStream>
 #include <QTimer>
 
 #include "MainWindow.hpp"
 
+QString readResourceText(const QString& resourcePath) {
+    QFile file(resourcePath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        return {};
+    }
+    QTextStream stream(&file);
+    return stream.readAll();
+}
+
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    const QString styleSheet =
+        readResourceText(":/styles/tokens.qss") + "\n" + readResourceText(":/styles/app.qss");
+    if (!styleSheet.isEmpty()) {
+        app.setStyleSheet(styleSheet);
+    }
 
     MainWindow window;
     window.show();
